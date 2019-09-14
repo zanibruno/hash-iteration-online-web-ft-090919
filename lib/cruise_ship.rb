564020e40508a122af4f2ece19a5aa3eb94c168c
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-14 11:30:19 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,10 +7,7 @@
 # }
 
 def select_winner(passengers)
-winner = " "
-passengers.each do |suite , name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name 
-  end 
-end 
+  
+  
 end
+
