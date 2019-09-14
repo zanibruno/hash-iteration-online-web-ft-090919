11170e1d566bb558a72f61e6e393819d1f89c24d
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-14 11:41:01 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,18 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end
 end
 
-
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |name, age|
+    if age < 12 
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
+    end 
+  end 
+end 
+  
