ab41d4ea94ffc8e959483a4e7c4a289d4deb62c3
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-14 11:37:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,10 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
+    if age < 12 
     puts "Happy Birthday #{name}! You are now #{age} years old!"
+  else
+    puts "You are too old for this."
   end
 end
 
